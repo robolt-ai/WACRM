867ff53c14bf57d62dfe09057b820c8303eddc5e
Aman sharma <87026675+Aman300@users.noreply.github.com>
2024-12-08 22:01:37 +0530

    model-update

--- src/models/order.js
+++ src/models/order.js
@@ -5,42 +5,43 @@ const schema = new mongoose.Schema(
   {
     customer_name: {
       type: String,
-      required: true,
+    //   required: true,
       trim: true, // removes whitespace from both ends of a string
     },
     customer_email: {
       type: String,
-      required: true,
+    //   required: true,
       trim: true,
     },
     customer_contact: {
       type: String,
-      required: true,
+    //   required: true,
       trim: true,
     },
     customer_address: {
       type: String,
-      required: true,
+    //   required: true,
       trim: true,
     },
     order_type: {
       type: String,
-      required: true,
+    //   required: true,
       trim: true,
     },
     order_date: {
       type: String,
-      required: true,
+    //   required: true,
+    default: new Date(),
       trim: true,
     },
     order_amount: {
       type: String,
-      required: true,
+    //   required: true,
       trim: true,
     },
     order_status: {
       type: String,
-      required: true,
+    //   required: true,
       trim: true,
     },
   },
